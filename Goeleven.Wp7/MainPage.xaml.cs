d32d02cba608d1e75634fbcd9d38d8e963e00295
Yves Goeleven <yves@goeleven.com>
2012-05-25 19:35:56 +0200

    Created navigation structure

--- Goeleven.Wp7/MainPage.xaml.cs
+++ Goeleven.Wp7/MainPage.xaml.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 
 namespace Goeleven.Wp7
@@ -22,8 +14,102 @@ namespace Goeleven.Wp7
 
             // Set the data context of the listbox control to the sample data
             DataContext = App.ViewModel;
-            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+            Loaded += MainPage_Loaded;
+
+            inboxGrid.Tap += inboxGrid_Tap;
+            projectsGrid.Tap += projectsGrid_Tap;
+            calendarGrid.Tap += calendarGrid_Tap;
+            marketplaceGrid.Tap += marketplaceGrid_Tap;
+
+            homeGrid.Tap += homeGrid_Tap;
+            gardenGrid.Tap += gardenGrid_Tap;
+            officeGrid.Tap += officeGrid_Tap;
+            outsideGrid.Tap += outsideGrid_Tap;
+            onthegoGrid.Tap += onthegoGrid_Tap;
+
+           
         }
+
+        void addActionMenuItem_Click(object sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Forms/AddAction.xaml", UriKind.Relative)));
+        }
+
+        void addInboxItemMenuItem_Click(object sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Forms/AddInboxItem.xaml", UriKind.Relative)));
+        }
+
+        void settings_Click(object sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Forms/Settings.xaml", UriKind.Relative)));
+        }
+
+        void gotoCurrentContext_Click(object sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Pages/NextActions.xaml", UriKind.Relative)));
+        }
+
+        void askQuestion_Click(object sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Forms/AskQuestion.xaml", UriKind.Relative)));
+        }
+
+        void addInboxItem_Click(object sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Forms/AddInboxItem.xaml", UriKind.Relative)));
+        }
+
+        void onthegoGrid_Tap(object sender, GestureEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Pages/NextActions.xaml", UriKind.Relative)));
+        }
+
+        void outsideGrid_Tap(object sender, GestureEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Pages/NextActions.xaml", UriKind.Relative)));
+        }
+
+        void officeGrid_Tap(object sender, GestureEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Pages/NextActions.xaml", UriKind.Relative)));
+        }
+
+        void gardenGrid_Tap(object sender, GestureEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Pages/NextActions.xaml", UriKind.Relative)));
+        }
+
+        void homeGrid_Tap(object sender, GestureEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Pages/NextActions.xaml", UriKind.Relative)));
+        }
+
+        void marketplaceGrid_Tap(object sender, GestureEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Pages/Marketplace.xaml", UriKind.Relative)));
+        }
+
+        void calendarGrid_Tap(object sender, GestureEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Pages/Calendar.xaml", UriKind.Relative)));
+        }
+
+        void projectsGrid_Tap(object sender, GestureEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Pages/Projects.xaml", UriKind.Relative)));
+        }
+
+        void inboxGrid_Tap(object sender, GestureEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Pages/Inbox.xaml", UriKind.Relative)));
+        }
+
+        private void questionGrid_Tap(object sender, GestureEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Pages/Question.xaml", UriKind.Relative)));
+        }
+
 
         // Load data for the ViewModel Items
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
@@ -33,5 +119,11 @@ namespace Goeleven.Wp7
                 App.ViewModel.LoadData();
             }
         }
+
+        
+
+        
+
+
     }
 }
